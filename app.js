4ce372215c73a4f7189eb2fc8c6b37df27eaf37f
riansopian <riansopianccd@gmail.com>
2016-07-01 13:51:14 +0700

    https2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
  */
 var express = require('express')
   , app = express()
-  , server = require('http').createServer(app)
+  , server = require('https').createServer(app)
   , io = require('socket.io').listen(server)
   , routes = require('./routes')
   , socket = require('socket.io')
